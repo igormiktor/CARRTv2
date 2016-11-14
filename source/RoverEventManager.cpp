bedfe45a69569224154692a14c0b414000fac3d1
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2016-11-13 22:04:30 -0500

    Add CARRT's dedicated event manager

---
+++ source/RoverEventManager.cpp
@@ -0,0 +1,384 @@
+/*
+ * EventManager.cpp
+ *
+
+ * An event handling system for Arduino.
+ *
+ * Author: igormt@alumni.caltech.edu
+ * Copyright (c) 2013 Igor Mikolic-Torreira
+ *
+ * Inspired by and adapted from the
+ * Arduino Event System library by
+ * Author: mromani@ottotecnica.com
+ * Copyright (c) 2010 OTTOTECNICA Italy
+ *
+ * This library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software
+ * Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser
+ * General Public License along with this library; if not,
+ * write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
+
+
+
+
+
+
+
+#define EVENTMANAGER_EVENT_QUEUE_SIZE		24
+
+
+#include "RoverEventManager.h"
+#include <util/atomic.h>
+
+
+#if EVENTMANAGER_DEBUG
+#define EVTMGR_DEBUG_PRINT( x )             Serial.print( x );
+#define EVTMGR_DEBUG_PRINTLN( x )           Serial.println( x );
+#define EVTMGR_DEBUG_PRINT_PTR( x )         Serial.print( reinterpret_cast<unsigned long>( x ), HEX );
+#define EVTMGR_DEBUG_PRINTLN_PTR( x )       Serial.println( reinterpret_cast<unsigned long>( x ), HEX );
+#else
+#define EVTMGR_DEBUG_PRINT( x )
+#define EVTMGR_DEBUG_PRINTLN( x )
+#define EVTMGR_DEBUG_PRINT_PTR( x )
+#define EVTMGR_DEBUG_PRINTLN_PTR( x )
+#endif
+
+
+
+
+namespace EventManager
+{
+
+    // EventQueue class used internally by EventManager
+    class EventQueue
+    {
+
+    public:
+
+        // Queue initializer (so we control when this happens
+        void init();
+
+        // Returns true if no events are in the queue
+        bool isEmpty();
+
+        // Returns true if no more events can be inserted into the queue
+        bool isFull();
+
+        // Actual number of events in queue
+        uint8_t getNumEvents();
+
+        // Tries to insert an event into the queue;
+        // Returns false if successful, true if the queue if full and the event cannot be inserted
+        //
+        // NOTE: if EventManager is instantiated in interrupt safe mode, this function can be called
+        // from interrupt handlers.  This is the ONLY EventManager function that can be called from
+        // an interrupt.
+        bool queueEvent( uint8_t eventCode, int16_t eventParam );
+
+        // Tries to extract an event from the queue;
+        // Returns true if successful, false if the queue is empty (the parameteres are not touched in this case)
+        bool popEvent( uint8_t* eventCode, int16_t* eventParam );
+
+    private:
+
+        // Event queue size.
+        // The maximum number of events the queue can hold is kEventQueueSize
+        // Increasing this number will consume 3 bytes of RAM for each unit.
+        static const uint8_t kEventQueueSize = EVENTMANAGER_EVENT_QUEUE_SIZE;
+
+        struct EventElement
+        {
+            int16_t param;  // each event has a single integer parameter
+            uint8_t code;   // each event is represented by an integer code
+        };
+
+        // The event queue
+        EventElement mEventQueue[ kEventQueueSize ];
+
+        // Index of event queue head
+        uint8_t mEventQueueHead;
+
+        // Index of event queue tail
+        uint8_t mEventQueueTail;
+
+        // Actual number of events in queue
+        uint8_t mNumEvents;
+    };
+
+
+    EventQueue  mHighPriorityQueue;
+    EventQueue  mLowPriorityQueue;
+
+ };
+
+ 
+ 
+ 
+//*********  INLINES   EventManager::EventQueue::  ***********
+
+inline bool EventManager::EventQueue::isEmpty()
+{
+    return ( mNumEvents == 0 );
+}
+
+
+inline bool EventManager::EventQueue::isFull()
+{
+    return ( mNumEvents == kEventQueueSize );
+}
+
+
+inline uint8_t EventManager::EventQueue::getNumEvents()
+{
+    return mNumEvents;
+}
+
+/*
+    RoverEventManager.cpp - The event management module for CARRT
+    
+    Copyright (c) 2016 Igor Mikolic-Torreira.  All right reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
+
+
+#include "RoverEventManager.h"
+#include <util/atomic.h>
+
+
+#if EVENTMANAGER_DEBUG
+
+#define EVTMGR_DEBUG_PRINT( x )             Serial.print( x );
+#define EVTMGR_DEBUG_PRINTLN( x )           Serial.println( x );
+#define EVTMGR_DEBUG_PRINT_PTR( x )         Serial.print( reinterpret_cast<unsigned long>( x ), HEX );
+#define EVTMGR_DEBUG_PRINTLN_PTR( x )       Serial.println( reinterpret_cast<unsigned long>( x ), HEX );
+
+#else
+
+#define EVTMGR_DEBUG_PRINT( x )
+#define EVTMGR_DEBUG_PRINTLN( x )
+#define EVTMGR_DEBUG_PRINT_PTR( x )
+#define EVTMGR_DEBUG_PRINTLN_PTR( x )
+
+#endif
+
+
+
+uint8_t EventManager::getNextEvent( uint8_t* eventCode, int16_t* param )
+{
+    if ( mHighPriorityQueue.popEvent( eventCode, param ) )
+    {
+        return 1;
+    }
+
+    // If no high-pri events handled (either because there are no high-pri events or
+    // because there are no listeners for them), then try low-pri events
+    if ( mLowPriorityQueue.popEvent( eventCode, param ) )
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+
+
+void EventManager::init()
+{
+    mHighPriorityQueue.init();
+    mLowPriorityQueue.init();
+}
+
+
+
+bool EventManager::isEventQueueEmpty( EventPriority pri )
+{
+    return ( pri == kHighPriority ) ? mHighPriorityQueue.isEmpty() : mLowPriorityQueue.isEmpty();
+}
+
+
+
+bool EventManager::isEventQueueFull( EventPriority pri )
+{
+    return ( pri == kHighPriority ) ? mHighPriorityQueue.isFull() : mLowPriorityQueue.isFull();
+}
+
+
+
+uint8_t EventManager::getNumEventsInQueue( EventPriority pri )
+{
+    return ( pri == kHighPriority ) ? mHighPriorityQueue.getNumEvents() : mLowPriorityQueue.getNumEvents();
+}
+
+
+
+bool EventManager::queueEvent( uint8_t eventCode, int16_t eventParam, EventPriority pri )
+{
+    return ( pri == kHighPriority ) ?
+        mHighPriorityQueue.queueEvent( eventCode, eventParam ) : mLowPriorityQueue.queueEvent( eventCode, eventParam );
+}
+
+
+
+
+
+
+
+/******************************************************************************/
+
+
+
+
+void EventManager::EventQueue::init() 
+{
+    mEventQueueHead = 0;
+    mEventQueueTail = 0;
+    mNumEvents = 0;
+
+    for ( uint8_t i = 0; i < kEventQueueSize; i++ )
+    {
+        mEventQueue[i].code = EventManager::kNullEvent;
+        mEventQueue[i].param = 0;
+    }
+}
+
+
+
+bool EventManager::EventQueue::queueEvent( uint8_t eventCode, int16_t eventParam )
+{
+    /*
+    * The call to noInterrupts() MUST come BEFORE the full queue check.
+    *
+    * If the call to isFull() returns FALSE but an asynchronous interrupt queues
+    * an event, making the queue full, before we finish inserting here, we will then
+    * corrupt the queue (we'll add an event to an already full queue). So the entire
+    * operation, from the call to isFull() to completing the inserting (if not full)
+    * must be atomic.
+    *
+    * Note that this race condition can only arise IF both interrupt and non-interrupt (normal)
+    * code add events to the queue.  If only normal code adds events, this can't happen
+    * because then there are no asynchronous additions to the queue.  If only interrupt
+    * handlers add events to the queue, this can't happen because further interrupts are
+    * blocked while an interrupt handler is executing.  This race condition can only happen
+    * when an event is added to the queue by normal (non-interrupt) code and simultaneously
+    * an interrupt handler tries to add an event to the queue.  This is the case that the
+    * cli() (= noInterrupts()) call protects against.
+    *
+    * Contrast this with the logic in popEvent().
+    *
+    */
+
+    // Because this function may be called from interrupt handlers, debugging is
+    // only available in when NOT in interrupt safe mode.
+
+
+    bool retVal = true;         // true means an error occurred
+
+    // ATOMIC BLOCK BEGIN
+    ATOMIC_BLOCK( ATOMIC_RESTORESTATE )
+    {
+        if ( !isFull() )
+        {
+            // Store the event at the tail of the queue
+            mEventQueue[ mEventQueueTail ].code = eventCode;
+            mEventQueue[ mEventQueueTail ].param = eventParam;
+
+            // Update queue tail value
+            mEventQueueTail = ( mEventQueueTail + 1 ) % kEventQueueSize;;
+
+            // Update number of events in queue
+            mNumEvents++;
+
+            retVal = false;
+        }
+    }
+    // ATOMIC BLOCK END
+
+    return retVal;
+}
+
+
+
+
+bool EventManager::EventQueue::popEvent( uint8_t* eventCode, int16_t* eventParam )
+{
+    /*
+    * The call to noInterrupts() MUST come AFTER the empty queue check.
+    *
+    * There is no harm if the isEmpty() call returns an "incorrect" TRUE response because
+    * an asynchronous interrupt queued an event after isEmpty() was called but before the
+    * return is executed.  We'll pick up that asynchronously queued event the next time
+    * popEvent() is called.
+    *
+    * If noInterrupts() is set before the isEmpty() check, we pretty much lock-up the Arduino.
+    * This is because popEvent(), via processEvents(), is normally called inside loop(), which
+    * means it is called VERY OFTEN.  Most of the time (>99%), the event queue will be empty.
+    * But that means that we'll have interrupts turned off for a significant fraction of the
+    * time.  We don't want to do that.  We only want interrupts turned off when we are
+    * actually manipulating the queue.
+    *
+    * Contrast this with the logic in queueEvent().
+    *
+    */
+
+    if ( isEmpty() )
+    {
+        return false;
+    }
+
+    // This function is NOT designed to called from interrupt handlers, so
+    // it is safe to turn interrupts on instead of restoring a previous state.
+    // ATOMIC BLOCK BEGIN
+    ATOMIC_BLOCK( ATOMIC_FORCEON )
+    {
+        // Pop the event from the head of the queue
+        // Store event code and event parameter into the user-supplied variables
+        *eventCode  = mEventQueue[ mEventQueueHead ].code;
+        *eventParam = mEventQueue[ mEventQueueHead ].param;
+
+        // Clear the event (paranoia)
+        mEventQueue[ mEventQueueHead ].code = EventManager::kNullEvent;
+
+        // Update the queue head value
+        mEventQueueHead = ( mEventQueueHead + 1 ) % kEventQueueSize;
+
+        // Update number of events in queue
+        mNumEvents--;
+    }
+
+    EVTMGR_DEBUG_PRINT( "popEvent() return " )
+    EVTMGR_DEBUG_PRINT( *eventCode )
+    EVTMGR_DEBUG_PRINT( ", " )
+    EVTMGR_DEBUG_PRINTLN( *eventParam )
+
+    return true;
+}
---
+++ source/RoverEventManager.h
@@ -0,0 +1,88 @@
+/*
+    RoverEventManager.h - The event management module for CARRT
+    
+    Copyright (c) 2016 Igor Mikolic-Torreira.  All right reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
+
+
+#ifndef EventManager_h
+#define EventManager_h
+
+#include <stdint.h>
+
+
+
+namespace EventManager
+{
+
+    enum
+    {
+        kNullEvent = 0,
+
+        // Timer events
+        kQuarterSecondTimerEvent,
+        kOneSecondTimerEvent,
+        kEightSecondTimerEvent,
+
+        // Nav update event
+        kNavUpdateEvent,
+        kNavDriftCorrectionEvent,
+
+        // Error event
+        kErrorEvent,
+
+        // Keypad events
+        kKeypadButtonHitEvent,
+
+        kLastEvent
+    };
+
+
+    // EventManager recognizes two kinds of events.  By default, events are
+    // are queued as low priority, but these constants can be used to explicitly
+    // set the priority when queueing events
+    //
+    // NOTE high priority events are always handled before any low priority events.
+    enum EventPriority { kLowPriority, kHighPriority };
+
+
+    void init();
+
+    // Returns true if no events are in the queue
+    bool isEventQueueEmpty( EventPriority pri = kLowPriority );
+
+    // Returns true if no more events can be inserted into the queue
+    bool isEventQueueFull( EventPriority pri = kLowPriority );
+
+    // Actual number of events in queue
+    uint8_t getNumEventsInQueue( EventPriority pri = kLowPriority );
+
+    // Tries to insert an event into the queue;
+    // returns true if successful, false if the
+    // queue if full and the event cannot be inserted
+    bool queueEvent( uint8_t eventCode, int16_t eventParam, EventPriority pri = kLowPriority );
+
+    // This function returns the next event
+    uint8_t getNextEvent( uint8_t* eventCode, int16_t* eventParam );
+
+};
+
+
+#endif
