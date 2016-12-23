6fe9e5fb8137266d5d8ff5053e9d41ab24370060
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2016-12-22 23:26:25 -0500

    Add base class for states for the state engine

---
+++ source/State.cpp
@@ -0,0 +1,59 @@
+/*
+    State.cpp - Base class for CARRT's states
+
+    Copyright (c) 2016 Igor Mikolic-Torreira.  All right reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
+
+#include "State.h"
+
+
+
+
+// onEntry() Set up the state
+void State::onEntry()
+{
+    // Default does nothing
+}
+
+
+
+// onExit() - tear-down the state.  It should delete the state, if appropriate
+void State::onExit()
+{
+    // Default deletes itself
+    delete this;
+}
+
+
+
+// onEvent() - deal with an event; return true to give other handlers an chance to
+// process the event; return false to allow no further event handling
+bool State::onEvent( uint8_t event, int16_t param )
+{
+    return true;
+}
+
+
+
+// Virtual Destructor
+State::~State()
+{
+    // Nothing to do; just need it to be virtual
+}
---
+++ source/State.h
@@ -0,0 +1,50 @@
+/*
+    State.h - Base class for CARRT's states
+
+    Copyright (c) 2016 Igor Mikolic-Torreira.  All right reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
+
+#ifndef State_h
+#define State_h
+
+#include <inttypes.h>
+
+
+class State
+{
+public:
+
+    // onEntry() Set up the state
+    virtual void onEntry();
+
+    // onExit() - tear-down the state.  It should delete the state, if appropriate
+    virtual void onExit();
+
+    // onEvent() - deal with an event; return true to give other handlers an chance to
+    // process the event; return false to allow no further event handling
+    virtual bool onEvent( uint8_t event, int16_t param );
+
+    // Virtual destructor
+    virtual ~State();
+
+};
+
+
+#endif
