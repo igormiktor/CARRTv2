bd8e7d7ae3fed8f0a9e5f4d9be1edebed0411c44
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2016-12-27 11:18:45 -0500

    Set file structure for coding Programmed Drive

---
+++ source/ProgDriveStates.cpp
@@ -0,0 +1,45 @@
+/*
+    ProgDriveStates.cpp - Programmed Drive States for CARRT
+
+    Copyright (c) 2016 Igor Mikolic-Torreira.  All right reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
+#if CARRT_INCLUDE_PROGDRIVE_IN_BUILD
+
+
+#include "ProgDriveStates.h"
+
+#include <avr/pgmspace.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif  // CARRT_INCLUDE_PROGDRIVE_IN_BUILD
---
+++ source/ProgDriveStates.h
@@ -0,0 +1,52 @@
+/*
+    ProgDriveStates.h - Programmed Drive States for CARRT
+
+    Copyright (c) 2016 Igor Mikolic-Torreira.  All right reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
+#if CARRT_INCLUDE_PROGDRIVE_IN_BUILD
+
+
+#ifndef ProgDriveStates_h
+#define ProgDriveStates_h
+
+
+
+#include "State.h"
+
+#include <stdint.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+#endif  // CARRT_INCLUDE_PROGDRIVE_IN_BUILD
