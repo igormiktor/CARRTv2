2048d4518ce1ec217b03d9c6c66af317d8b8e783
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2017-12-07 20:26:22 -0500

    Expose a function to access the error state (convenient way to report an error in a function that returns a state)

--- source/MainProcess.h
+++ source/MainProcess.h
@@ -39,6 +39,7 @@ namespace MainProcess
     void runEventLoop();
     void changeState( State* newState );
     void postErrorEvent( int errorCode );
+    State* getErrorState( int errorCode );
 };
 
 
